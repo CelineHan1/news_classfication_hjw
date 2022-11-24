08a15e03cb0653fdd1dd9ee2f9f11da50de6c83f
CelineHan1 <ys4572@gmail.com>
2022-11-24 10:50:56 +0900

    headline crawling comp

--- job01_crawling_headline.py
+++ job01_crawling_headline.py
@@ -24,7 +24,11 @@ for i in range(6):
     #print(title_tags[0].text)
     titles=[]
     for title_tag in title_tags:
-        titles.append(title_tag.text)
+        title=title_tag.text
+        #print(title)
+        title=re.compile('[^가-힣 ]').sub(' ',title)    #^가-힣0-9a-zA-Z
+        #print(title)
+        titles.append(title)
     df_section_titles=pd.DataFrame(titles, columns=['titles'])
     df_section_titles['category']=category[i]
     df_titles=pd.concat([df_titles, df_section_titles],axis='rows',ignore_index=True)
@@ -32,3 +36,5 @@ for i in range(6):
 print(df_titles)
 print(df_titles.category.value_counts())
 df_titles.to_csv('./crawlind_data/naver_headline_news_{}.csv'.format(datetime.datetime.now().strftime('%Y%m%d')),index=False)
+
+
